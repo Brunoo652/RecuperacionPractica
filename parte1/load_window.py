b74437dc67b56d9dfc35b6e6882a34eb93a4db8b
Brunoo652 <breboredoa@fpcoruna.afundacion.org>
2023-03-06 11:14:46 +0100

    segunda parta sin comprobar

--- parte1/load_window.py
+++ parte1/load_window.py
@@ -1,6 +1,12 @@
+import shutil
+
 import gi
+
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk, GLib
+import requests
+import threading
+from main_window import MainWindow2
 
 
 class LoadWindow(Gtk.Window):
@@ -20,3 +26,34 @@ class LoadWindow(Gtk.Window):
         self.box.pack_start(self.label, True, True, 0)
         self.box.pack_start(self.spinner, True, True, 0)
         self.add(self.box)
+
+       # self.load_json()
+        self.launch_load()
+
+    def launch_load(self):
+        thread = threading.Thread(target=self.load_json, args=())
+        thread.start()
+
+    def load_json(self):
+        response = requests.get('https://github.com/CarlosAfundacion/EXAMEN/blob/main/games.json')
+        json_list = response.json()
+
+        result = []
+
+        for json_item in json_list:
+            name = json_item.get("name")
+            descripcion = json_item.get("descripcion")
+            image_url = json_item.get("imagen_url")
+            r = requests.get("imagen_url", stream=True)
+            with open("temp.png", 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+            image = Gtk.image.new_from_file("temp.png")
+            result.append({"nombre": name}, {"descripcion": descripcion}, {"gtk_image": image})
+
+        GLib.idle_add(self.start_main_window, result)
+
+    def start_main_window(self, loaded_items_list):
+        win = MainWindow2(loaded_items_list)
+        win.show_all()
+        # self.disconnect_by_func(Gtk.main_quit)
+        self.close()
---
+++ parte1/main_window.py
@@ -0,0 +1,29 @@
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+from cell import Cell
+
+
+class MainWindow2(Gtk.Window):
+    flowbox = Gtk.FlowBox()
+
+    def __init__(self, data_source):
+        super().__init__()
+        self.connect("destroy", Gtk.main_quit)
+        self.set_border_width(15)
+        self.set_default_size(400, 400)
+
+        header= Gtk.HeaderBar(title="Catálogo")
+        header.set_subtitle("Catálogo 2023")
+        header.props.show_close_button = True
+
+        self.set_titlebar(header)
+
+        scrolled = Gtk.ScrolledWindow()
+        scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        scrolled.add(self.flowbox)
+        self.add(scrolled)
+
+        for item in data_source:
+            cell = Cell(item.get("name"), item.get("gtk_image"))
+            self.flowbox.add(cell)
